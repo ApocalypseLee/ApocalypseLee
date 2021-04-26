69a7efc3207eed022559c7c9065b0d38bc9d1613
Apocalypse <1025548262@qq.com>
2021-04-26 16:10:09 +0800

    added:login

---
+++ app/src/main/java/com/yt/apps/model/LoginResult.kt
@@ -0,0 +1,11 @@
+package com.yt.apps.model
+
+import com.yt.apps.Widgets.LoggedInUserView
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
