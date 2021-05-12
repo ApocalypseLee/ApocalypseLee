afd94167077e84eab39aedde05a06f2f9e2062a6
Apocalypse <1025548262@qq.com>
2021-05-12 19:21:54 +0800

    no message

---
+++ app/src/main/java/com/yt/apps/Widgets/DetailItem.java
@@ -0,0 +1,13 @@
+package com.yt.apps.Widgets;
+
+import android.content.Context;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class DetailItem extends ConstraintLayout {
+    public DetailItem(Context context) {
+        super(context);
+    }
+
+
+}
